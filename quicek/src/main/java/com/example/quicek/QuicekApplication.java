a085a2aed0c25ce461c6f25865bba0847bf1bd1b
roesn <rosen@qq.com>
2023-12-16 16:30:03 +0800

    just

---
+++ quicek/src/main/java/com/example/quicek/QuicekApplication.java
@@ -0,0 +1,13 @@
+package com.example.quicek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuicekApplication.class, args);
+    }
+
+}
---
+++ quicek/src/test/java/com/example/quicek/QuicekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quicek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
